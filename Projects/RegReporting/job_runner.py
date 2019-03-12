392bb51d234b3d5cce3e3d8984be644e6a9faeab
Michal Zelezniak <michal.zelezniak@gmail.com>
2019-03-11 21:51:47 -0400

    requirements update

--- Projects/RegReporting/job_runner.py
+++ Projects/RegReporting/job_runner.py
@@ -1,18 +1,22 @@
 '''
-Job runner script for Reg Reporting webapp 
+Data flow job execution script for Regulatory Reporting (Manual Adjustments) application
+The script is ran daily, it reads the content of 'data flows' table and executes data transfers as per definition. 
 '''
 
 import sys
 
 # High Level Requirements
+
 # 1. read  data_flow_job table in RegReporting database (ms sql server)
 #       for each data flow job:
-#           a) pull data based on return -> report_data
-#           b) initiate data flow tasks based on return-report and job definition definition
+#           a) pull data based on return -> report_data (data shape is part of report definition)
+#           b) initiate data flow tasks based on data destination definition definition for the flow
 #
+
 # 2. data is stored in 3 columns i.e. element_id, element_value, additional_attributes (json object)
 #    create a csv output file from above and send it out to its destination 
 #
+
 # 3. supported data destinations
 #    a) windows share drive e.g. \\server\\folder\\ 
 #    b) sftp e.g. host (pre setup required e.g. key exchange )
@@ -24,7 +28,10 @@ def main(argv):
         pass
     except ValueError as exception:
         print(exception)
+    except:
+        print('Unhandled error in main()')
 
 
 if __name__ == "__main__":
     main(sys.argv)
+
