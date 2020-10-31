cac018817adc108b7e3bd8dddb8350679beb88c3
Michal Zelezniak <michal.zelezniak@gmail.com>
2020-10-31 12:24:18 -0400

    kk

--- Projects/folder-sorter/monitor.py
+++ Projects/folder-sorter/monitor.py
@@ -1,22 +1,38 @@
 import watchdog.events 
 import watchdog.observers 
 import time 
+import click
+
+
+@click.command()
+@click.option('--date', default=1, help='Sort files into date subfolders')
+@click.option('--name', prompt='Sort files into data type folders',
+              help='The person to greet.')
+def sort_files(count, name):
+    """Sort files into corresponding subfolders based on selected option"""
+    pass
+
+
+@click.command()
+@click.option('--date', default=1, help='Put incoming files into date subfolders')
+@click.option('--name', prompt='Put incoming files into data type subfolders',
+              help='The person to greet.')
+def monitor_folder(count, name):
+    """Monitor a folder and sort incoming files into corresponding subfolders based on selected option"""
+    pass
 
 
 class Handler(watchdog.events.PatternMatchingEventHandler): 
     def __init__(self): 
         # Set the patterns for PatternMatchingEventHandler 
-        watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=['*.csv'], 
-                                                             ignore_directories=True, case_sensitive=False) 
+        watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=['*.csv'], ignore_directories=True, case_sensitive=False) 
   
     def on_created(self, event): 
         print("Watchdog received created event - % s." % event.src_path) 
         # Event is created, you can process it now 
   
     def on_modified(self, event): 
-        print("Watchdog received modified event - % s." % event.src_path) 
-        # Event is modified, you can process it now
-
+        pass #print("Watchdog received modified event - % s." % event.src_path) 
   
 if __name__ == "__main__":
     src_path = "./data/"
